49c34711acc505e7650019138c9d2eb387f2d651
刘启明 <liuqiming@szsinostar.com>
2019-02-25 10:06:58 +0800

    News新闻绑定数据OK，图片问题已处理

--- app/src/main/java/com/sinostar/assistant/ui/BlogMessage/BlogNewsAdapter.java
+++ app/src/main/java/com/sinostar/assistant/ui/BlogMessage/BlogNewsAdapter.java
@@ -6,6 +6,8 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.Message;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -33,37 +35,24 @@ public class BlogNewsAdapter extends BaseQuickAdapter<BlogNewsModel, BaseViewHol
 
     @Override
     protected void convert(BaseViewHolder helper, BlogNewsModel item) {
-        //Bitmap bitmap=ImageUtils.getimage(item.getTopicIcon());
         //将每一个需要赋值的id和对应的数据绑定
+
+        ImageView imageView=helper.getView( R.id.login_title_images );
         helper.setText( R.id.test_item_title, item.getTitle() )
-                .setText( R.id.test_item_message, item.getSummary() )
-                .setText( R.id.title_date, item.getDateAdded() )
-               // .setImageBitmap(R.id.login_title_image,bitmap)
-                .setText( R.id.title_author, "阅读量：" + item.getViewCount() );
+               .setText( R.id.test_item_message, item.getSummary() )
+               .setText( R.id.title_date, item.getDateAdded() )
+               .setText( R.id.title_author, "阅读量：" + item.getViewCount() );
+                //.setImageBitmap(R.id.login_title_image,getImageBitmap(item.getTopicIcon()));
+
+        try
+        {
+            Picasso.get().load( item.getTopicIcon() ).into( imageView );
+        }
+        catch(Throwable e)
+        {
+            throw e;
+        }
 
         //setImageView(item.getTopicIcon(),helper);
-    }
-
-    public void setImageView(String url,final BaseViewHolder helper){
-        if (loadtarget == null) loadtarget = new Target() {
-            @Override
-            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                helper.setImageBitmap( R.id.login_title_image,bitmap );
-            }
-
-            @Override
-            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-
-            }
-
-            @Override
-            public void onPrepareLoad(Drawable placeHolderDrawable) {
-
-            }
-
-
-        };
-
-        Picasso.get().load(url).into(loadtarget);
     }
 }
