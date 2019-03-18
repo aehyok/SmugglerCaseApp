5e3ec31b439c4d4652b68c7698279b23be5ce6f5
刘启明 <liuqiming@szsinostar.com>
2019-03-18 16:08:58 +0800

    弹出录入框，录入界面简单整理，待处理弹出登录界面

--- app/src/main/java/com/sinostar/assistant/ui/basecontrol/baseControlActivity.java
+++ app/src/main/java/com/sinostar/assistant/ui/basecontrol/baseControlActivity.java
@@ -1,26 +1,42 @@
 package com.sinostar.assistant.ui.basecontrol;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.siberiadante.customdialoglib.CustomDialog;
 import com.sinostar.assistant.R;
+
+import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import static com.sinostar.assistant.base.ApplicationUtil.getContext;
 
 public class baseControlActivity extends AppCompatActivity {
     @BindView( R.id.spinner )
     Spinner rSpinner;
     @BindView(R.id.button)
     Button rButton;
+
+
+    int TAG1401 = 1000;
+    int TAG1402 = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
@@ -36,7 +52,8 @@ public class baseControlActivity extends AppCompatActivity {
 
 
     }
-    @OnClick({R.id.button,R.id.buttonDialog})
+
+    @OnClick({R.id.button,R.id.buttonDialog,R.id.loginDialogButton,R.id.login2DialogButton})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.button:
@@ -73,6 +90,100 @@ public class baseControlActivity extends AppCompatActivity {
                         .create();
                 alertDialog1.show();
                 break;
+            case R.id.loginDialogButton:
+                loginDialogButtonClick();
+                break;
+            case R.id.login2DialogButton:
+                login2DialogButtonClick();
+                break;
         }
+    }
+
+    public void loginDialogButtonClick()
+    {
+        final EditText editText=new EditText( this );
+        AlertDialog.Builder editDialog = new AlertDialog.Builder(this);
+        editDialog.setTitle("弹出登录页面");
+        editDialog.setIcon(R.mipmap.ic_launcher);
+
+        //设置dialog布局
+        editDialog.setView(editText);
+
+        //设置按钮
+        editDialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(baseControlActivity.this,
+                                editText.getText().toString().trim(),Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                });
+
+        editDialog.create().show();
+    }
+
+
+    public void login2DialogButtonClick()
+    {
+        WindowManager wm = (WindowManager) getContext()
+                .getSystemService(Context.WINDOW_SERVICE);
+
+        int width = wm.getDefaultDisplay().getWidth();
+        int height = wm.getDefaultDisplay().getHeight();
+
+
+        final RelativeLayout relativeLayout222=new RelativeLayout( this );
+        relativeLayout222.setMinimumWidth( width );
+        relativeLayout222.setPadding( 40,40,40,40 );
+        final RelativeLayout relativeLayout=new RelativeLayout( this );
+        relativeLayout.setMinimumWidth( width );
+        //relativeLayout.setGravity( Gravity.FILL_VERTICAL );
+
+
+        final LinearLayout linearLayout=new LinearLayout( this );
+        linearLayout.setMinimumWidth(width);
+        final TextView textView=new TextView(this);
+        textView.setText("用户名:");
+        final EditText editText=new EditText( this );
+        editText.setId(TAG1401 );
+        editText.setWidth( 650 );
+        linearLayout.addView( textView );
+        linearLayout.addView( editText );
+        relativeLayout.addView( linearLayout );
+
+        final RelativeLayout relativeLayout1=new RelativeLayout( this );
+        relativeLayout1.setMinimumWidth( width );
+        relativeLayout1.setPadding( 0,40,0,0 );
+        final LinearLayout linearLayout1=new LinearLayout( this );
+        linearLayout1.setMinimumWidth(width);
+        final TextView textView1=new TextView(this);
+        textView1.setText("密码:");
+        final EditText editText1=new EditText( this );
+        editText1.setId(TAG1402 );
+        editText1.setWidth( 650 );
+        linearLayout1.addView( textView1 );
+        linearLayout1.addView( editText1 );
+        relativeLayout1.addView( linearLayout1 );
+//        final EditText editText2=new EditText( this );
+//        linearLayout.addView( editText );
+//        linearLayout.addView( editText2 );
+        AlertDialog.Builder editDialog = new AlertDialog.Builder(this);
+        editDialog.setTitle("弹出登录页面");
+        editDialog.setIcon(R.mipmap.ic_launcher);
+        relativeLayout222.addView( relativeLayout );
+        relativeLayout222.addView( relativeLayout1 );
+        //设置dialog布局
+        editDialog.setView(relativeLayout222);
+        //设置按钮
+        editDialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(baseControlActivity.this,
+                        editText.getText().toString().trim(),Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        editDialog.create().show();
     }
 }
